8e21a103fb9eea80fd10b49933e592da379afffd
Nethmal Gunawardhana <107822302+nethmalgunawardhana@users.noreply.github.com>
2024-10-01 15:37:43 +0530

    Revert "update"

--- src/pages/Settingspage.jsx
+++ src/pages/Settingspage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
+import SettingsContent from '../components/Settingcontent';
 
 const SettingsPage = () => (
-  <div className="flex flex-col h-screen">
+  <div className="flex flex-col min-h-screen">
     <Navbar />
-    <div className="flex-grow overflow-hidden">
+    <div className="flex flex-grow">
       <Sidebar />
+      <SettingsContent />
     </div>
   </div>
 );
