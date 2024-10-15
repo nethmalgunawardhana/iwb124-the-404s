15a2b4bcbb159850e767f3dd6abc904576042476
H.K Sahan Tharaka <sahantharakaw2p2@gmail.com>
2024-10-15 22:27:21 +0530

    modified:   src/App.jsx 	new file:   src/components/EventCard.jsx 	new file:   src/pages/EventPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+// src/App.jsx
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import SignUpPage from "./pages/Signup"; // Updated to import SignupPage
+import SignUpPage from "./pages/Signup"; 
 import FogotPasswordForm from "./components/fogotpassword";
 import SettingsPage from "./pages/Settingspage";
 import Verifycode from "./components/verifycode";
-import HomePage from "./pages/HomePage"; // Import HomePage component
+import HomePage from "./pages/HomePage"; 
 import Profile from "./components/Profile";
 import EventCreator from "./pages/EventCreator";
 import EventEdit from "./components/EventEdit";
@@ -15,7 +16,8 @@ import OrganizationVerification from "./pages/org-verify";
 import PVerification from "./pages/person-verify";
 import AdminDashboard from "./pages/Admin/admindashboard";
 import BrowseEventsPage from './pages/BrowseEventsPage';
-import Login from './pages/Login'; // Import the new Login component
+import Login from './pages/Login'; 
+import EventPage from './pages/EventPage'; // Import the new EventPage component
 import "./App.css";
 
 function App() {
@@ -25,21 +27,23 @@ function App() {
     <BrowserRouter>
       <div>
         <Routes>
-          <Route path="/" element={<HomePage />} /> {/* HomePage is the landing route */}
-          <Route path="/login" element={<Login />} /> {/* Updated to use Login */}
-          <Route path="/signup" element={<SignUpPage />} /> {/* Updated to use SignupPage */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUpPage />} />
           <Route path="/fogotpassword" element={<FogotPasswordForm />} />
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="/verifycode" element={<Verifycode />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/eventcreator" element={<EventCreator />} />
-          <Route path="/browse-events" element={<BrowseEventsPage />} /> {/* Add Browse Events Page route */}
-          <Route path="/evenedit" element={<EventEdit />} />
+          <Route path="/browse-events" element={<BrowseEventsPage />} />
+          <Route path="/eventedit" element={<EventEdit />} />
           <Route path="/verify" element={<AccountVerification />} />
           <Route path="/eventdashboard" element={<EventDashboard />} />
           <Route path="/organization-verification" element={<OrganizationVerification />} />
           <Route path="/personal-verification" element={<PVerification />} />
           <Route path="/admin" element={<AdminDashboard />} />
+          {/* Add the new route for EventPage */}
+          <Route path="/event-page" element={<EventPage />} /> {/* New route for Event Page */}
         </Routes>
       </div>
     </BrowserRouter>
